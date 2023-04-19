eb1b579be2760c0e9e5bb381cd5bbd16d7e1b7fd
sakshineema <sakshineema2001@gmail.com>
2023-04-19 17:53:28 +0530

    second commit

--- src/app/pages/user/instructions/instructions.component.ts
+++ src/app/pages/user/instructions/instructions.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { QuizzesService } from 'src/app/services/quizzes.service';
 
 @Component({
   selector: 'app-instructions',
   templateUrl: './instructions.component.html',
   styleUrls: ['./instructions.component.css']
 })
-export class InstructionsComponent {
+export class InstructionsComponent implements OnInit{
 qid:any;
-
-constructor(private route:ActivatedRoute){
+quizz:any;
+constructor(private route:ActivatedRoute,private quiz:QuizzesService){
 
 }
+  ngOnInit(): void {
+   this.qid = this.route.snapshot.params['qid'];
+   
+   this.quiz.getSingleQuiz(this.qid).subscribe(
+    (data:any) => {
+      console.log(data)
+      this.quizz=data;
+   },(error) =>{
+       console.log("something Went Wrong");
+   })
+  }
 }
